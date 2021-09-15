859c55bd6b1d36f26dc2ce0128de5529a8be9478
theokabir <theogame4@gmail.com>
2021-09-15 18:20:22 -0300

    estilo da página de curriculo v1

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,16 +1,29 @@
 import Profile from "../components/resume/Profile";
 import Body from "../components/resume/Body";
+import bg from './../assets/space_wpp.jpg'
 
 const Resume = ({ language }) => {
   return ( 
     <div className="
-    w-full lg:w-9/12 mx-auto py-8
-    md:grid md:grid-cols-3 grid-cols-1 font-page text-white bg-blueGray-700
-    ">
-      <Profile language={language} />
-      <Body language={language} />
+    w-full
+    bg-cover bg-fixed bg-center bg-no-repeat
+    " 
+    style={{
+      backgroundImage: `url(${bg})`,
+    }}>
+      <div className="
+      w-full lg:w-9/12 mx-auto py-8
+      md:grid md:grid-cols-3 grid-cols-1 font-page text-white
+      opacity-80
+      "
+      style={{
+        backgroundColor: "rgba(51, 65, 85, 0.8)"
+      }}>
+        <Profile language={language} />
+        <Body language={language} />
+      </div>
     </div>
   );
 }
  
-export default Resume;
+export default Resume;                                  
